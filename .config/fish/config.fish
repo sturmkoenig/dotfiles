be5dba040f713d9658cd574dd9dff56f1f287102
linus lauer <linus.lauer@codecentric.de>
2024-04-04 10:36:28 +0200

    added config shortcuts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,9 @@ fish_vi_key_bindings
 alias v=nvim
 alias lzy=lazygit
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias lzycfg='lzy --git-dir $HOME/.cfg/ --work-tree $HOME'
+alias vcfg='v ~/.config/nvim/'
+alias fishcfg='v ~/.config/fish/config.fish'
 
 
 function code
